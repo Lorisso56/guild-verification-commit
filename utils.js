1a3e140adf8227c1c7e5bef4a4600d3b6201ce84
Lorisso56 <lorisso56@outlook.com>
2026-03-09 21:44:49 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T18:44:25.155Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-09T18:44:49.822Z
+Commit: Resolve null pointer exception in user service
