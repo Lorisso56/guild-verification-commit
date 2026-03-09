b0e6b4a08b7c6be00ff6e62265b990cd510d45a7
Lorisso56 <lorisso56@outlook.com>
2026-03-09 21:46:06 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T18:45:59.913Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-09T18:46:06.981Z
+Commit: Patch security vulnerability in authentication
