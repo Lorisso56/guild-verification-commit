bbb7f9596fbf626de1f9ab31779b750918b865c2
Lorisso56 <lorisso56@outlook.com>
2026-03-09 21:45:59 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T18:45:46.490Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-09T18:45:59.913Z
+Commit: Fix broken links in documentation
