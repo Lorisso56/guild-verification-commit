87eb8171b3de6d8442d4ba271bcc99f6426ceafe
Lorisso56 <lorisso56@outlook.com>
2026-03-09 21:47:27 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T18:46:55.753Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-09T18:47:27.716Z
+Commit: Add multi-language support
