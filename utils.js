0e8498436363afbe5accb3970edf45af26a61086
Lorisso56 <lorisso56@outlook.com>
2026-03-09 21:47:31 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T18:46:44.220Z
-Commit: Add multi-language support
+Timestamp: 2026-03-09T18:47:31.412Z
+Commit: Resolve timezone issues in date handling
